3cb0f7ee823d75704adeab4315f25bbcbe2c859e
LinFeng1997 <244732635@qq.com>
2020-04-21 11:21:53 +0800

    lint

--- test/sort.test.ts
+++ test/sort.test.ts
@@ -3,6 +3,6 @@ const sortColors = require('../src/sort/75.sortColors');
 describe('sort', () => {
   test('75. sort colors', () => {
     expect(sortColors([2, 0, 2, 1, 1, 0])).toEqual([0, 0, 1, 1, 2, 2]);
-    expect(sortColors([2,0,1])).toEqual([0, 1,2]);
+    expect(sortColors([2, 0, 1])).toEqual([0, 1, 2]);
   });
 });
