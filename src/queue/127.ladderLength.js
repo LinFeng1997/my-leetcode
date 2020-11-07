0d0ac67a498474cdce4efc684ebba562a056b8e7
LinFeng1997 <244732635@qq.com>
2020-11-08 00:06:41 +0800

    chore: ladderLengthRaw

--- src/queue/127.ladderLength.js
+++ src/queue/127.ladderLength.js
@@ -34,4 +34,40 @@ var ladderLength = function (beginWord, endWord, wordList) {
     return 0;
 };
 
+var ladderLengthRaw = function (beginWord, endWord, wordList) {
+    let queue = [];
+    queue.unshift([beginWord, 1]);
+    const visited = new Map();
+    visited.set(beginWord, true);
+
+    while (queue.length) {
+        const [word, step] = queue.shift();
+
+        if (word === endWord) return step;
+
+        for (let i = 0; i < wordList.length; i++) {
+            let cur = wordList[i];
+
+            if (!visited.has(cur) && transformCheck(word, cur)) {
+                queue.push([cur, step + 1]);
+                visited.set(cur, true);
+            }
+        }
+    }
+
+    return 0;
+};
+
+function transformCheck(str1, str2) {
+    if (str1.length !== str2.length) return false;
+    let differences = 0;
+    for (let i = 0; i < str1.length && differences < 2; i++) {
+        if (str1[i] !== str2[i]) {
+            differences++;
+        }
+    }
+    return differences === 1;
+}
+
+
 module.exports = ladderLength;
