393dd4857db221d564d00a42929898fc60265bb8
LinFeng1997 <244732635@qq.com>
2020-11-22 23:03:12 +0800

    chore: 优化 79，进来的时候就判断，而不是移动后判断

--- src/backtracking/79.exist.js
+++ src/backtracking/79.exist.js
@@ -14,33 +14,34 @@ var exist = function (board, word) {
     }
 
     function dfs(index, row, col) {
-        if (index === word.length) {
-            res = true;
-            return;
-        }
-        if (word.length === 1 && board[row][col] === word[0]) {
-            res = true;
-            return;
-        }
-        const direction = getDirection(row, col);// 走四个方向
-        Object.keys(direction).forEach(key => {
-            const [i, j] = direction[key];
+        const char = board[row][col];
 
-            if (i < 0 || j < 0 || i >= m || j >= n) {
+        if (index === word.length - 1) {
+            if (char === word[index]) {
+                res = true;
                 return;
             }
-            if (isUsed(i, j)) {
-                return;
-            }
+        }
 
-            if (board[i][j] === word[index]) {
-                visited[`${i}-${j}`] = true;
+        if (char === word[index]) {
+            visited[`${row}-${col}`] = true;
+
+            const direction = getDirection(row, col);// 走四个方向
+            Object.keys(direction).forEach(key => {
+                const [i, j] = direction[key];
+
+                if (i < 0 || j < 0 || i >= m || j >= n) {
+                    return;
+                }
+                if (isUsed(i, j)) {
+                    return;
+                }
 
                 dfs(index + 1, i, j);
+            });
 
-                visited[`${i}-${j}`] = false; // 复原
-            }
-        });
+            visited[`${row}-${col}`] = false;
+        }
 
     }
 
@@ -69,53 +70,3 @@ function getDirection(i, j) {
 }
 
 module.exports = exist;
-
-// function test79(board,word) {
-// let res = false;
-// let paths = [];
-//
-// function isUsed(i,j) {
-// return paths.some(item => item.i === i && item.j === j)
-// };
-//
-// function dfs(str,row,col) {
-// if (!str) {
-// res = true;
-// return;
-// }
-// let direction = getDirection(row,col);
-// // 走四个方向
-// Object.keys(direction).forEach(key => {
-// const [i,j] = direction[key];
-//
-// if (i < 0 || j < 0 || i >= board.length) return
-// if (isUsed(i,j)) return
-// let char = board[i][j];
-// // debugger;
-//
-// if (char === str[0]) {
-// paths.push({
-// i,
-// j
-// });
-//
-// dfs(str.slice(1),i,j)
-//
-// paths.pop();
-// }
-// })
-// }
-//
-//
-// for (let i = 0;i<board.length;i++) {
-// for (let j = 0;j<board[i].length;j++) {
-// dfs(word,i,j);
-//
-// if (res === true) {
-// return res;
-// }
-// }
-// }
-//
-// return res;
-// }
