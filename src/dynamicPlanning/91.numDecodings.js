04e9487848587c9e0ec59ccdf58071ffaec4a28c
LinFeng1997 <244732635@qq.com>
2020-11-28 12:34:13 +0800

    refactor: 91

--- src/dynamicPlanning/91.numDecodings.js
+++ src/dynamicPlanning/91.numDecodings.js
@@ -9,28 +9,27 @@ var numDecodings = function(s) {
     memo[0] = 1;
     memo[1] = 1;
 
-    // 实际写代码要把索引都往前一位
-    for (let i = 1; i < s.length; i++) {
-        if (s[i - 1] === "0") {
-            if (s[i] === "0") {
+    for (let i = 2; i <= s.length; i++) {
+        if (s[i - 2] === "0") {
+            if (s[i - 1] === "0") {
                 return 0;
             } else {
-                memo[i + 1] = memo[i];
+                memo[i] = memo[i - 1];
             }
         } else {
-            let num = +s.substr(i - 1, 2);
+            let num = +s.substr(i - 2, 2);
 
-            if (s[i] === "0") {
+            if (s[i - 1] === "0") {
                 if (num > 26) {
                     return 0
                 } else {
-                    memo[i + 1] = memo[i - 1]
+                    memo[i] = memo[i - 2]
                 }
             } else {
                 if (num > 26) {
-                    memo[i + 1] = memo[i]
+                    memo[i] = memo[i - 1]
                 } else {
-                    memo[i + 1] = memo[i] + memo[i - 1]
+                    memo[i] = memo[i - 1] + memo[i - 2]
                 }
             }
         }
@@ -47,5 +46,5 @@ var numDecodings = function(s) {
 // 条件判断：a0 = 0, a1 =  0,dp[n] = 0  200
 // a0 = 0, a1 != 0, dp[n] = dp[n - 1]  206
 // a0 != 0, a1 = 0, 若 a0a1 > 26, dp[n] = 0,否则 dp[n] = dp[n - 2] 260 220
-// a0 != 0, a1 !=  0,若 a0a1 > 26, dp[n] = dp[n - 1],否则 dp[n] = dp[n -1] + dp[n - 2] 226 227
+// a0 != 0, a1 != 0,若 a0a1 > 26, dp[n] = dp[n - 1],否则 dp[n] = dp[n -1] + dp[n - 2] 226 227
 module.exports = numDecodings;
