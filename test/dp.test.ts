f59152ad0a9bd39f62fbed7e90f706fa20ae376d
LinFeng1997 <244732635@qq.com>
2020-12-08 22:44:43 +0800

    chore: lint

--- test/dp.test.ts
+++ test/dp.test.ts
@@ -167,15 +167,15 @@ describe('dynamic planning', () => {
   test('300.lengthOfLIS', () => {
     const lengthOfLIS = require('../src/dynamicPlanning/300.lengthOfLIS');
 
-    expect(lengthOfLIS([10,9,2,5,3,7,101,18])).toBe(4);
+    expect(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])).toBe(4);
   });
 
   test('376.wiggleMaxLength', () => {
     const wiggleMaxLength = require('../src/dynamicPlanning/376.wiggleMaxLength');
     //
-    expect(wiggleMaxLength([1,7,4,9,2,5])).toBe(6);
-    expect(wiggleMaxLength([1,17,5,10,13,15,10,5,16,8])).toBe(7);
-    expect(wiggleMaxLength([1,2,3,4,5,6,7,8,9])).toBe(2);
-    expect(wiggleMaxLength([0,0])).toBe(1);
+    expect(wiggleMaxLength([1, 7, 4, 9, 2, 5])).toBe(6);
+    expect(wiggleMaxLength([1, 17, 5, 10, 13, 15, 10, 5, 16, 8])).toBe(7);
+    expect(wiggleMaxLength([1, 2, 3, 4, 5, 6, 7, 8, 9])).toBe(2);
+    expect(wiggleMaxLength([0, 0])).toBe(1);
   });
 });
--- test/greedy.test.ts
+++ test/greedy.test.ts
@@ -1,15 +1,15 @@
 describe('greedy', () => {
-    test('455.findContentChildren', () => {
-        const findContentChildren = require('../src/greedy/455.findContentChildren');
-        //
-        expect(findContentChildren([1,2,3],[1,1])).toBe(1);
-        expect(findContentChildren([1,2],[1,2,3])).toBe(2);
-    });
+  test('455.findContentChildren', () => {
+    const findContentChildren = require('../src/greedy/455.findContentChildren');
+    //
+    expect(findContentChildren([1, 2, 3], [1, 1])).toBe(1);
+    expect(findContentChildren([1, 2], [1, 2, 3])).toBe(2);
+  });
 
-    test('392.isSubsequence', () => {
-        const isSubsequence = require('../src/greedy/392.isSubsequence');
-        //
-        expect(isSubsequence("abc","ahbgdc")).toBe(true);
-        expect(isSubsequence("axc","ahbgdc")).toBe(false);
-    });
+  test('392.isSubsequence', () => {
+    const isSubsequence = require('../src/greedy/392.isSubsequence');
+    //
+    expect(isSubsequence('abc', 'ahbgdc')).toBe(true);
+    expect(isSubsequence('axc', 'ahbgdc')).toBe(false);
+  });
 });
